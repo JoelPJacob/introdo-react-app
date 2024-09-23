a5ce0fda7d4b2482c7767d7ac66ad789f8e3ce4d
joel <joel.jacob@roadelabs.com>
2024-09-23 12:05:41 +0530

    Header section code optimization

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,35 +5,53 @@ import Logo from '../assets/icons/app-logo.svg';
 import { ReactComponent as ProfileIcon } from '../assets/icons/avatar-logo.svg';
 
 const Header = () => {
+  const menuItems = [
+    { id: 1, name: 'Products', hasDropdown: true },
+    { id: 2, name: 'Solutions', hasDropdown: true },
+    { id: 3, name: 'Pricing', hasDropdown: false },
+    { id: 4, name: 'Partner Program', hasDropdown: false },
+    { id: 5, name: 'Resources', hasDropdown: true },
+    { id: 6, name: 'About Us', hasDropdown: true }
+  ];
+
   return (
     <div>
+      {/* Top bar */}
       <div className="top-box">
         <p>
-          Happy Employees, Effortless HR: Experience the introdo Advantage. <span className="bold-text">Try it for Free!</span>
+          Happy Employees, Effortless HR: Experience the Introdo Advantage.
+          <span className="bold-text"> Try it for Free!</span>
         </p>
       </div>
+
+      {/* Navigation bar */}
       <nav>
         <div className="nav-container">
+          {/* Left side of navigation (logo and menu) */}
           <div className="nav-left">
             <ul className="nav-list">
               <li>
-                <a href="/products">
-                  <img src={Logo} alt="App Logo" className="nav-logo" />
-                  <span className="nav-logo-text">Introdo</span>
-                </a>
+                {/* App logo */}
+                <img src={Logo} alt="App Logo" className="nav-logo" />
+                <span className="nav-logo-text">Introdo</span>
               </li>
-              <li><a href="/products">Products <img src={DownArrow} alt="Down Arrow" className="nav-icon" /></a></li>
-              <li><a href="/solutions">Solutions <img src={DownArrow} alt="Down Arrow" className="nav-icon" /></a></li>
-              <li><a href="/pricing">Pricing</a></li>
-              <li><a href="/partner-program">Partner Program</a></li>
-              <li><a href="/resources">Resources <img src={DownArrow} alt="Down Arrow" className="nav-icon" /></a></li>
-              <li><a href="/about">About Us <img src={DownArrow} alt="Down Arrow" className="nav-icon" /></a></li>
+              {/* Menu items using map */}
+              {menuItems.map((item, index) => (
+                <li key={index}>
+                  {item.name}
+                  {item.hasDropdown && <img src={DownArrow} alt="Down Arrow" className="nav-icon" />}
+                </li>
+              ))}
             </ul>
           </div>
+
+          {/* Right side of navigation (language and login buttons) */}
           <div className="nav-right">
+            {/* Language selection button */}
             <button className="header-button">
               EN <img src={DownArrow} alt="Down Arrow" className="button-icon" />
             </button>
+            {/* Login button with profile icon */}
             <button className="header-button">
               Log In <ProfileIcon className="profile-icon" />
             </button>
@@ -42,6 +60,6 @@ const Header = () => {
       </nav>
     </div>
   );
-}
+};
 
 export default Header;
